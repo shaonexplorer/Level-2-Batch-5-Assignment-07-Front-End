4c6bbf203d1c4a387dfe4a569b36bcbe9219ac30
shaonexplorer <shaonexplorer@gmail.com>
2025-10-05 10:49:44 +0600

    disabling ISR

--- src/app/(public)/blogs/[id]/page.tsx
+++ src/app/(public)/blogs/[id]/page.tsx
@@ -1,36 +1,40 @@
 import { Blogpost1 } from "@/components/blogpost1";
 
-export const generateStaticParams = async () => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/post`,
-    { cache: "no-store" }
-  );
-  const posts = await res.json();
-  return posts.data.map((post: { id: number }) => {
-    return { id: post.id.toString() };
-  });
-};
+// export const generateStaticParams = async () => {
+//   const res = await fetch(
+//     `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/post`,
+//     { cache: "no-store" }
+//   );
+//   const posts = await res.json();
+//   return posts.data.map((post: { id: number }) => {
+//     return { id: post.id.toString() };
+//   });
+// };
 
-export const generateMetadata = async ({
+// export const generateMetadata = async ({
+//   params,
+// }: {
+//   params: { id: string };
+// }) => {
+//   const { id } = params;
+
+//   const res = await fetch(
+//     `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/post/${id}`
+//   );
+//   const post = await res.json();
+
+//   return {
+//     title: post.data.title,
+//     description: post.data.content,
+//   };
+// };
+
+async function BlogDetailsPage({
   params,
 }: {
-  params: { id: string };
-}) => {
-  const { id } = params;
-
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/post/${id}`
-  );
-  const post = await res.json();
-
-  return {
-    title: post.data.title,
-    description: post.data.content,
-  };
-};
-
-async function BlogDetailsPage({ params }: { params: { id: string } }) {
-  const { id } = params;
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/post/${id}`
   );
